bca430352dbe0d5cb501913e68f8c44badc7e2fa
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2021-05-25 11:19:05 +0100

    Restore useMirrors property
    Fails to apply on server with an unresolved reference for
    AgentCheckoutPolicy and checkoutPolicy.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,7 +6,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.toId
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot.AgentCheckoutPolicy
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 version = "2020.2"
@@ -19,7 +18,7 @@ project {
         id(vcsName.toId())
         name = vcsName
         url = vcsUrl
-        checkoutPolicy = AgentCheckoutPolicy.NO_MIRRORS
+        useMirrors = false
     }
     vcsRoot(vcsRoot)
 
